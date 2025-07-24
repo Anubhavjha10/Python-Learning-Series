3d121a50f0d9202625207a483be3fbafeea74109
Kumar Anubhav <anubhavjha424@gmail.com>
2025-07-24 13:42:44 +0530

    Add files via upload

---
+++ 2D Collection.py
@@ -0,0 +1,7 @@
+fruits = ["apple", "banana", "cherry"]
+vegetables = ["carrots", "celery", "Patatoes"]
+meats =  ["Chicken", "fish", "turkey"]
+
+grocries = [fruits, vegetables, meats]
+
+print(grocries[1 ][1])
