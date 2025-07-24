2ed58c732dc827911455542aff15e07e3be149ba
Kumar Anubhav <anubhavjha424@gmail.com>
2025-07-24 12:37:26 +0530

    Add files via upload

---
+++ lists,set,tuples.py
@@ -0,0 +1,13 @@
+#collections = single "variable" used to store multiple values
+# List = [] ordered and chargeable. Duplicate OK
+#set = {} unordered and imutable, but Add/Remove OK. NO duplicates
+#Tuples = () ordered and unchangeable. Duplicate OK. Faster
+
+fruits = ["apple", "banana", "cherry"]          # Lists
+print(fruits[0])
+
+car = {"BMW", "AUDI","TATA", "MAHINDRA"}
+print(car)
+
+bike = ("Bajaj","TVS","Suzuki","Hero")
+print(bike)
