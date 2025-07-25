569d27165035e18d249278798b2e186c430d78ee
Kumar Anubhav <anubhavjha424@gmail.com>
2025-07-25 10:43:38 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,16 @@
+# function = A block of reusable code
+#            place () after the function name to invoke it
+
+#def happy_birthday(name):
+    #print(f"Happy Birthday! {name}")
+   # print("You are happy!")
+  #  print("Happy Birthday Again! ")
+ #   print()
+#
+#happy_birthday("Bro")
+
+def display_invoice(username, amount, due_date):
+    print(f"Hello {username}")
+    print(f"Your bill is: {amount:} is due: {due_date} ")
+
+display_invoice("John", "100", "01/02")
